f60e31360f5e33409b79c32d6b54063d64e3f12f
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-23 15:22:04 +0500

    number

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-export {};
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// i computer will generate a random number
+// ii user input for guessing the number
+// iii compare user input with computer generated number and show result
+const randomnumber = 15;
+const answer = await inquirer.prompt([
+    {
+        name: "userguessnumber",
+        type: "number",
+        message: "Please guess a number: ",
+    },
+]);
+console.log(answer);
--- game.ts
+++ game.ts
@@ -1,6 +1,17 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([{
-    
-}])
+// i computer will generate a random number
+// ii user input for guessing the number
+// iii compare user input with computer generated number and show result
+
+const randomnumber = 15;
+const answer = await inquirer.prompt([
+  {
+    name: "userguessnumber",
+    type: "number",
+    message: "Please guess a number: ",
+  },
+]);
+
+console.log(answer);
